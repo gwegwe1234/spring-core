89c5fb6286ac7e42018c57e4a9417d523cd491ba
gwegwe1234 <gwangtae5860@daum.net>
2023-06-10 05:36:15 +0900

    primary 적용

--- src/main/java/com/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/hello/core/discount/RateDiscountPolicy.java
@@ -2,12 +2,13 @@ package com.hello.core.discount;
 
 import com.hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import static com.hello.core.member.Grade.VIP;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private final int discountPercent = 10;
